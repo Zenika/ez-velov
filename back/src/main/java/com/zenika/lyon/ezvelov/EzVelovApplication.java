24265151fddc2e6d74107d67b6cb6ae340c51000
Rémy D <remy.david@zenika.com>
2022-04-12 15:38:36 +0200

    initialisation du back(spring)

---
+++ back/src/main/java/com/zenika/lyon/ezvelov/EzVelovApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.lyon.ezvelov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzVelovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzVelovApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/zenika/lyon/ezvelov/EzVelovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.lyon.ezvelov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzVelovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
