57055912b831e8088d6e2ada6dc4bb4f17123c6a
Rémy D <remy.david@zenika.com>
2022-04-15 16:20:26 +0200

    TU methode findAll

---
+++ back/src/test/java/com/zenika/lyon/ezvelov/infrastructure/utilisateur/UtilisateurStoreTest.java
@@ -0,0 +1,51 @@
+package com.zenika.lyon.ezvelov.infrastructure.utilisateur;
+
+
+import com.zenika.lyon.ezvelov.domain.utilisateur.Utilisateur;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UtilisateurStoreTest {
+
+    @InjectMocks
+    public UtilisateurStore utilisateurStore;
+
+    @Mock
+    public UtilisateurJpaRepository utilisateurJpaRepository;
+
+    @Spy
+    public UtilisateurEntityMapper utilisateurEntityMapper;
+
+    @Test
+    public void findAll_shouldReturnEmptyList_whenFindAllReturnEmptyList() {
+        //given
+
+        //when
+        utilisateurStore.findAll();
+
+        //then
+        verify(utilisateurEntityMapper, never()).utilisateurEntityToUtilisateur(any());
+    }
+
+    @Test
+    public void findAll_shouldReturnOnlyOneUtilisateur_whenListUtilisateurSizeIsOne() {
+        //given
+        UtilisateurEntity utilisateurEntity = new UtilisateurEntity();
+        when(utilisateurJpaRepository.findAll()).thenReturn(List.of(utilisateurEntity));
+
+        //when
+        utilisateurStore.findAll();
+
+        //then
+        verify(utilisateurEntityMapper).utilisateurEntityToUtilisateur(utilisateurEntity);
+    }
+}
