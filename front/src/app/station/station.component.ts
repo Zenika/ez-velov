56d3eea61bb969d5f2d1b2c2ecf033f372bf188b
Oonag <loic.duchet@zenika.com>
2022-05-03 11:21:55 +0200

    Passage des fichiers de style de css à scss

--- front/src/app/station/station.component.ts
+++ front/src/app/station/station.component.ts
@@ -5,7 +5,7 @@ import {StationService} from "../station.service";
 @Component({
   selector: 'app-station',
   templateUrl: './station.component.html',
-  styleUrls: ['./station.component.css']
+  styleUrls: ['./station.component.scss']
 })
 export class StationComponent implements OnInit {
 
--- front/src/app/welcome/welcome.component.ts
+++ front/src/app/welcome/welcome.component.ts
@@ -5,7 +5,7 @@ import {Utilisateur} from "../utilisateur";
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
 
