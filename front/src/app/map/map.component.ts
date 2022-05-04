76e42ca031907fecd8c8ade104f0cbbc6e5777e3
RemyDavid0 <103437146+RemyDavid0@users.noreply.github.com>
2022-05-04 15:17:20 +0200

    [feat-57] ajout d'une map de lyon (#19)
    [feat-57] ajout d'une map de lyon

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -4,7 +4,7 @@ import * as mapboxgl from "mapbox-gl";
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
 
@@ -12,31 +12,11 @@ export class MapComponent implements OnInit {
 
   ngOnInit(): void {
     let map = new mapboxgl.Map({
-      style: "mapbox://styles/scorpion6912/cl2iy7ygp004n15mxim5z3l9k",
+      style: "mapbox://styles/scorpion6912/cl2rcl9oe006i14o1hqa9s4n8",
       accessToken: 'pk.eyJ1Ijoic2NvcnBpb242OTEyIiwiYSI6ImNsMmVoMXFwbjAwbm0zaW1rdjUzcnRrZ2IifQ.bp5c4G0lq1UsWSRJbLnfVg',
       container: 'map',
       center: [4.835659, 45.764043],
       zoom: 12
     })
-
-    map.on('click', (event) => {
-      const features = map.queryRenderedFeatures(event.point, {
-        layers: ['lyon-velov']
-      });
-
-      if (!features.length) {return;}
-
-      const feature = features[0];
-      new mapboxgl.Popup({ offset: [0,-15] })
-        .setLngLat(event.lngLat)
-        .setHTML(
-          `<div id="popup">
-            <h3>${feature.id} i'm the id</h3><p>génial ça marche</p>
-          </div>
-          <style>#popup {background-color: antiquewhite; padding: 0; margin: 0;}</style>
-          `
-        )
-        .addTo(map);
-    });
   }
 }
