68f4895fb751fbdd5441a2ed5ba38c4deb5ef046
Oonag <loic.duchet@zenika.com>
2022-05-04 15:06:19 +0200

    Le token jcdecaux est vaulté

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ez-velov
+# Ez-velov
+
+Ez-Velov est une application de localisation de Velov et d'itinéraire intelligente. L'essayer c'est l'adopter !  
+
+## Lancement coté back
+Renseigner la clef d'api jcdecaux lors du lancement en argument :
+* --jcdecaux.token=???
--- back/src/main/java/com/zenika/lyon/ezvelov/infrastructure/provider/station/StationProvider.java
+++ back/src/main/java/com/zenika/lyon/ezvelov/infrastructure/provider/station/StationProvider.java
@@ -1,7 +1,10 @@
 package com.zenika.lyon.ezvelov.infrastructure.provider.station;
 
 import com.zenika.lyon.ezvelov.domain.station.IRequestStationStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,14 +14,16 @@ public class StationProvider implements IRequestStationStore {
 
     private final RestTemplate restTemplate;
 
+    @Value("${jcdecaux.token}")
+    private String jcDecauxToken;
+
     public StationProvider(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
     @Override
     public String getAllStations() {
         return restTemplate.getForObject(
-                "https://api.jcdecaux.com/vls/v3/stations?contract=lyon" +
-                        "&apiKey=20e619f2897e7fe10ea90a724e30dfd152ad4a59",
+                String.format("https://api.jcdecaux.com/vls/v3/stations?contract=lyon&apiKey=%s", jcDecauxToken),
                 String.class);
     }
 }
