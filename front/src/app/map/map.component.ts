4ada125d37ab4cd0f8e9faa57d22236cef9f6664
Oonag <loic.duchet@zenika.com>
2022-06-08 11:34:58 +0200

    Renommage fonctions

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -10,33 +10,28 @@ import {Station} from "../station";
 })
 export class MapComponent implements OnInit {
 
-  private stations?: Station[];
-
-  private geolocator?: mapboxgl.GeolocateControl;
-
   constructor(private stationService: StationService) { }
 
   ngOnInit(): void {
-    let map = new mapboxgl.Map({
-      style: "mapbox://styles/scorpion6912/cl2rcl9oe006i14o1hqa9s4n8",
-      accessToken: 'pk.eyJ1Ijoic2NvcnBpb242OTEyIiwiYSI6ImNsMmVoMXFwbjAwbm0zaW1rdjUzcnRrZ2IifQ.bp5c4G0lq1UsWSRJbLnfVg',
-      container: 'map',
-      center: [4.835659, 45.764043],
-      zoom: 12
-    });
-
-    this.addPointsOnMap(map);
-
-
-    this.geolocator = this.initGeolocate(map);
-
-    this.loadUserGeoloc(this.geolocator, map);
+    let map = this.initMap();
+    this.initStations(map);
+    let geolocator = this.initGeolocator(map);
+    this.triggerGeolocator(map, geolocator);
   }
 
-  addPointsOnMap(map: mapboxgl.Map): void{
+  private initMap(): mapboxgl.Map {
+    return new mapboxgl.Map({
+        style: "mapbox://styles/scorpion6912/cl2rcl9oe006i14o1hqa9s4n8",
+        accessToken: 'pk.eyJ1Ijoic2NvcnBpb242OTEyIiwiYSI6ImNsMmVoMXFwbjAwbm0zaW1rdjUzcnRrZ2IifQ.bp5c4G0lq1UsWSRJbLnfVg',
+        container: 'map',
+        center: [4.835659, 45.764043],
+        zoom: 12
+      });
+  }
+
+  private initStations(map: mapboxgl.Map): void {
     this.stationService.getAllStations()
       .subscribe(stations => {
-        this.stations = stations
         stations.forEach((station) => {
           const {longitude, latitude} = station?.positionDto;
           new mapboxgl.Marker().setLngLat([longitude,latitude]).addTo(map)
@@ -44,8 +39,8 @@ export class MapComponent implements OnInit {
       });
   }
 
-  initGeolocate(map: mapboxgl.Map): mapboxgl.GeolocateControl {
-    const geolocate = new mapboxgl.GeolocateControl({
+  private initGeolocator(map: mapboxgl.Map): mapboxgl.GeolocateControl {
+    const geolocator = new mapboxgl.GeolocateControl({
       positionOptions: {
         enableHighAccuracy: true,
       },
@@ -53,13 +48,13 @@ export class MapComponent implements OnInit {
       showUserHeading: true,
       showAccuracyCircle: true
     })
-    map.addControl(geolocate);
-    return geolocate;
+    map.addControl(geolocator);
+    return geolocator;
   }
 
-  loadUserGeoloc(geolocate: mapboxgl.GeolocateControl, map: mapboxgl.Map): void {
+  private triggerGeolocator(map: mapboxgl.Map, geolocator: mapboxgl.GeolocateControl): void {
     map.on('load', function() {
-      geolocate.trigger();
+      geolocator.trigger();
     });
   }
 }
