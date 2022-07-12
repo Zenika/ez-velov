920ab2c6a3f84530695c81a404181cca744cb2a4
Oonag <loic.duchet@zenika.com>
2022-07-13 00:51:58 +0200

    ÃCorrection approximative du code de merde du stagiaire

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -5,7 +5,6 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import {timer} from 'rxjs';
 import {Position} from "../position";
 import {MapboxService} from "./mapbox.service";
-import {MapService} from "./map.service";
 
 @Component({
   selector: 'app-map',
@@ -13,6 +12,10 @@ import {MapService} from "./map.service";
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+
+  private readonly TIMER_GEOLOCALISATION = 2000;
+
+  private readonly TIMER_FLYING_MAP = 3000;
 
   private rechercheDestination?: boolean;
 
@@ -24,13 +27,16 @@ export class MapComponent implements OnInit {
 
   public geolocalisation!: mapboxgl.GeolocateControl;
 
-  private readonly TIMER_GEOLOCALISATION = 2000;
+  public positionDepart!: Position;
 
-  private readonly TIMER_FLYING_MAP = 3000;
+  public positionArrivee?: Position;
+  
+  public dureeTrajet?: string;
+
+  public instructionTrajet?: string;
 
   constructor(private stationService: StationService,
-              private mapboxService:MapboxService,
-              public mapService: MapService) {
+              public mapboxService: MapboxService) {
   }
 
   ngOnInit(): void {
@@ -49,8 +55,11 @@ export class MapComponent implements OnInit {
   }
 
   public affichageTrajetStationLaPlusProche(): void {
-    this.stationService.findStationLaPlusProche(this.mapService.coordsStart).subscribe(station => {
-      this.mapService.coordsEnd = [station.positionDto.longitude, station.positionDto.latitude];
+    this.stationService.findStationLaPlusProche(this.positionDepart).subscribe(station => {
+      this.positionArrivee = {
+        longitude: station.positionDto.longitude,
+        latitude: station.positionDto.latitude,
+      };
       this.creerRoute();
     })
   }
@@ -58,7 +67,7 @@ export class MapComponent implements OnInit {
   public setCoordsOfStartToPosition(): void {
     this.geolocalisation.trigger();
     timer(this.TIMER_GEOLOCALISATION).subscribe(() => {
-      this.mapService.coordsStart = {
+      this.positionDepart = {
         longitude: this.map.getCenter().lng,
         latitude: this.map.getCenter().lat
       }
@@ -70,15 +79,18 @@ export class MapComponent implements OnInit {
     this.rechercheDestination = true;
     this.geolocalisation.trigger();
     timer(this.TIMER_GEOLOCALISATION).subscribe(() => {
-      this.mapService.coordsEnd = [this.map.getCenter().lng, this.map.getCenter().lat]
+      this.positionArrivee = {
+        longitude: this.map.getCenter().lng,
+        latitude: this.map.getCenter().lat,
+      };
       this.isPositionOk = true;
     });
   }
 
   private creerRoute(): void {
-    this.mapService.removeRoute(this.map);
+    this.mapboxService.removeRoute(this.map);
     this.afficherDureeTrajet = true;
-    this.mapService.getRouteInformations(this.map)
+    this.calculerInformationsItineraire();
   }
 
 
@@ -102,7 +114,10 @@ export class MapComponent implements OnInit {
         if (rechercheTrajet?.checked) {
           this.rechercheDestination = true;
           new mapboxgl.Marker().setLngLat([this.map.getCenter().lng, this.map.getCenter().lat]).addTo(this.map);
-          this.mapService.coordsEnd = [this.map.getCenter().lng, this.map.getCenter().lat];
+          this.positionArrivee = {
+            longitude: this.map.getCenter().lng,
+            latitude: this.map.getCenter().lat,
+          };
           this.creerRoute();
         }
       })
@@ -114,7 +129,7 @@ export class MapComponent implements OnInit {
 
     document.querySelector('#rechercheDepart')?.appendChild(rechercheDepart.onAdd(this.map));
     timer(this.TIMER_FLYING_MAP).subscribe(() => {
-      this.mapService.coordsStart = {
+      this.positionDepart = {
         longitude: this.map.getCenter().lng,
         latitude: this.map.getCenter().lat
       }
@@ -127,7 +142,7 @@ export class MapComponent implements OnInit {
         positionRecherche = new mapboxgl.Marker().setLngLat([this.map.getCenter().lng, this.map.getCenter().lat])
         positionRecherche.addTo(this.map)
         if (trajet?.checked) {
-          this.mapService.coordsStart = {
+          this.positionDepart = {
             longitude: this.map.getCenter().lng,
             latitude: this.map.getCenter().lat
           }
@@ -162,5 +177,26 @@ export class MapComponent implements OnInit {
 
   private infosStations(capacity: number, placeDispo: number): string {
     return 'capacité : ' + capacity + '<br>' + 'places disponibles : ' + placeDispo
+  }
+
+  private calculerInformationsItineraire() {
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.responseType = 'json';
+    xmlHttpRequest.open('GET', this.mapboxService.buildInformationsItineraireUrl(this.positionDepart, this.positionArrivee), true);
+    xmlHttpRequest.onload = () => {
+      let apiDirectionResponse = xmlHttpRequest.response;
+      let duration = apiDirectionResponse.routes[0].duration / 60;
+      let stepsOfRoad = apiDirectionResponse.routes[0].legs[0].steps;
+      let coords = apiDirectionResponse.routes[0].geometry;
+      let instructionsTrajet = '';
+      stepsOfRoad.forEach(function (step: any) {
+        instructionsTrajet += step.maneuver.instruction;
+      });
+      this.instructionTrajet = instructionsTrajet;
+
+      this.dureeTrajet = duration.toFixed(2)
+      this.mapboxService.dessinerItineraire(this.map, coords);
+    };
+    xmlHttpRequest.send();
   }
 }
--- front/src/app/map/mapbox.service.ts
+++ front/src/app/map/mapbox.service.ts
@@ -2,21 +2,75 @@ import {Injectable} from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 import * as MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { Position } from '../position';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapboxService {
 
-  private mapboxAPIKey = 'pk.eyJ1Ijoic2NvcnBpb242OTEyIiwiYSI6ImNsMmVoMXFwbjAwbm0zaW1rdjUzcnRrZ2IifQ.bp5c4G0lq1UsWSRJbLnfVg';
+  private readonly MAPBOX_API_KEY = 'pk.eyJ1Ijoic2NvcnBpb242OTEyIiwiYSI6ImNsMmVoMXFwbjAwbm0zaW1rdjUzcnRrZ2IifQ.bp5c4G0lq1UsWSRJbLnfVg';
 
   constructor() {
+  }
+
+  public buildInformationsItineraireUrl(positionDepart: Position, positionArrivee?: Position) {
+    const trajetAVelo = document.getElementById('trajetAVelo') as HTMLInputElement | null;
+    const trajetAPied = document.getElementById('trajetAPied') as HTMLInputElement | null;
+    const coordoneeArrivee = positionArrivee
+    ? positionArrivee.longitude + ',' + positionArrivee.latitude
+    : null;
+    const coords =
+    positionDepart.longitude + ','
+    + positionDepart.latitude + ';'
+    +  coordoneeArrivee;
+
+    const typeTrajet = trajetAPied?.checked && !trajetAVelo?.checked
+      ? 'walking/'
+      : 'cycling/';
+
+    return 'https://api.mapbox.com/directions/v5/mapbox/'
+      + typeTrajet
+      + coords
+      + '?geometries=geojson&steps=true&access_token=pk.eyJ1Ijoic2NvcnBpb242OTEyIiwiYSI6ImNsMmVo'
+      + 'MXFwbjAwbm0zaW1rdjUzcnRrZ2IifQ.bp5c4G0lq1UsWSRJbLnfVg';
+  }
+
+  public dessinerItineraire(map: mapboxgl.Map, coords: any) {
+    map.addLayer({
+      "id": "route",
+      "type": "line",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": coords
+        }
+      },
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "red",
+        "line-width": 8,
+        "line-opacity": 0.8
+      }
+    });
+  }
+
+  public removeRoute(map: mapboxgl.Map) {
+    if (map.getSource('route')) {
+      map.removeLayer('route');
+      map.removeSource('route');
+    }
   }
 
   public initMap(): mapboxgl.Map {
     return new mapboxgl.Map({
       style: 'mapbox://styles/mapbox/streets-v11',
-      accessToken: this.mapboxAPIKey,
+      accessToken: this.MAPBOX_API_KEY,
       container: 'map',
       center: [4.835659, 45.764043],
       zoom: 12
@@ -25,7 +79,7 @@ export class MapboxService {
 
   public initRechercheDestination(map: mapboxgl.Map): MapboxGeocoder {
     return new MapboxGeocoder({
-      accessToken: this.mapboxAPIKey,
+      accessToken: this.MAPBOX_API_KEY,
       placeholder: 'Destination',
       marker: false,
       mapboxgl: map,
@@ -35,7 +89,7 @@ export class MapboxService {
 
   public initRechercheDepart(map: mapboxgl.Map): MapboxGeocoder {
     return new MapboxGeocoder({
-      accessToken: this.mapboxAPIKey,
+      accessToken: this.MAPBOX_API_KEY,
       placeholder: 'Départ',
       marker: false,
       mapboxgl: map,
